de7d58a53691253b232698528fe62ed3b72ebd77
Przemek <przem1361@gmail.com>
2017-11-28 12:04:36 +0100

    Updated the comic book detail layour

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,7 +13,7 @@ namespace ComicBookGallery.Controllers
         {
             var comicBook = new ComicBook()
             {
-                SeriesTitle = "The Amazing Spiderman",
+                SeriesTitle = "The Amazing Spider man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
